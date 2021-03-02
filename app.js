282f091fd8816e90616f56d41ad7797bc532b5b6
Rashotech <rashotech@gmail.com>
2021-03-02 21:06:39 +0100

    server file

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const middleware = require('./middleware');
+const path = require('path');
+const bodyParser = require('body-parser');
+var session = require('express-session');
+const mongoose = require('./db');
+
+const PORT = 5000;
+
+app.set("view engine", "pug");
+app.set("views, views")
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}))
+
+// Routes
+const loginRoutes = require('./routes/loginRoutes');
+const registerRoutes = require('./routes/registerRoutes');
+const logout = require('./routes/logout');
+
+// Api Routes
+const apiPostRoutes = require('./routes/api/posts');
+
+app.use(loginRoutes);
+app.use(registerRoutes);
+app.use(logout);
+app.use('/api', apiPostRoutes)
+
+app.get('/', middleware.requireLogin, (req, res) => {
+    payload = {
+        "pageTitle": "Home",
+        "userLoggedIn": req.session.user,
+        "userLoggedInJs": JSON.stringify(req.session.user)
+    }
+    res.render('home', payload);
+})
+app.listen(PORT, () => {
+    console.log(`App running at Port ${PORT}`);
+});
